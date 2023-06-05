107746389c7a0fac686483abf8b06ae8f0b2c0ff
sgdevreal <135599602+sgdevreal@users.noreply.github.com>
2023-06-05 12:15:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
 import logging.handlers
 import os
-
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import requests
 
 logger = logging.getLogger(__name__)
@@ -17,7 +19,8 @@ logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
 try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
+    SOME_SECRET = os.environ["EMAIL_ME"]
+    EMAIL_PASSWORD_ME = os.environ["EMAIL_PASSWORD_ME"]
 except KeyError:
     SOME_SECRET = "Token not available!"
     #logger.info("Token not available!")
@@ -26,9 +29,29 @@ except KeyError:
 
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
+    
+    def send_email(sender_email, sender_password, receiver_email, subject, body):
+        # Create a multipart message and set headers
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+        # Add body to the email
+        message.attach(MIMEText(body, "plain"))
+
+        # Create SMTP session for sending the email
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()  # Enable secure connection
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+
+    # Provide the necessary information
+    sender_email = SOME_SECRET
+    sender_password = EMAIL_PASSWORD_ME
+    receiver_email = SOME_SECRET
+    subject = "Hello from Python!"
+    body = "This is the body of the email."
+
+    # Call the send_email function
+    send_email(sender_email, sender_password, receiver_email, subject, body)
