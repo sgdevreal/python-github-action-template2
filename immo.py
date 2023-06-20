25f9c25d5a6e1309bbb0b766078c49a8d2565f2d
sgdevreal <135599602+sgdevreal@users.noreply.github.com>
2023-06-20 21:04:06 +0200

    Create immo.py

---
+++ immo.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+import datetime
+import pandas as pd
+from pandas import json_normalize
+from time import sleep
+import random 
+import numpy as np
+
+
+# In[2]:
+
+
+flag = True
+page = 1
+date = datetime.datetime.now().strftime("%d")
+df_list =[]
+while flag:
+    print(f'page : {page} - time : {datetime.datetime.now()}')
+    sleep(random.randint(1,5))
+    response = requests.get(f'https://www.immoweb.be/fr/search-results/maison-et-appartement/a-vendre/bruxelles/arrondissement?countries=BE&page={page}&orderBy=relevance')
+    with open(f'rawFiles/{page}_{date}.txt', 'w') as f:
+        f.write(response.text)
+    print(len(response.text))
+    if len(response.text) == 0:
+        flag = False
+        break
+    if page > 250:
+        flag = False
+        break
+    page+=1
+    meta = []
+    #print(response.json()['results'])
+    df = json_normalize(response.json(), record_path='results')
+    df_list.append(df)    
+
+
+# In[3]:
+
+
+full_df = pd.concat(df_list)
+
+
+# In[4]:
+
+
+full_df["extractDate"] = datetime.datetime.now()
+full_df["extractYear"] = datetime.datetime.now().strftime("%Y")
+full_df["extractMonth"] = datetime.datetime.now().strftime("%m")
+full_df["extractDay"] = datetime.datetime.now().strftime("%d")
+
+
+# In[5]:
+
+
+full_df
+
+
+# In[8]:
+
+
+df.to_excel(f"immo/outputfolder/database_{datetime.datetime.today().strftime('%Y%m%d')}.xlsx")
+
