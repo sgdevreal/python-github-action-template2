217424699f04e14409ff5eb96973d7768c79cbe2
sgdevreal <135599602+sgdevreal@users.noreply.github.com>
2024-12-01 15:00:36 +0100

    Update immo.py

--- immo.py
+++ immo.py
@@ -47,7 +47,10 @@ def main(t,p):
     url = f'https://www.immoweb.be/fr/search-results/{t}/a-vendre/?countries=BE&provinces={p}&page=1&orderBy=relevance'
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     headers = {'User-Agent': user_agent}
-    response = requests.get(url, headers=headers).json()
+    response = requests.get(url, headers=headers)
+    print(response.status_code)
+    print(response.text)  
+    response = response.json()
     if response['marketingCount']%30==0:
         pages = (response['marketingCount']//30)
     else:
